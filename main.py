09d29a2de25a0a8544e7b984bd3e7e6ba360d08e
PhoenixPlanet <ytw0113@naver.com>
2019-05-03 17:42:51 +0900

    base physics

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 
 pygame.init()
 
-# 게임 화면 설정
+# 게임 화면 설정(확인해본 결과 (1440, 960), (1280, 1080) 정도 되면 연산속도가 눈에 띌 정도로 느려짐)
 screen = (720, 480)
 window = pygame.display.set_mode(screen)
 pygame.display.set_caption("Project Dobbys")
@@ -12,7 +12,9 @@ pygame.display.set_caption("Project Dobbys")
 
 # 캐릭터, 타일 크기 설정
 playerSize = (33, 48)
-tileSize = (15, 15)
+dummyMargin = int(playerSize[0] / 4.7)
+dummyPlayerSize = (playerSize[0] - dummyMargin*2, playerSize[1])
+tileSize = screen
 
 # FPS
 clock = pygame.time.Clock()
@@ -21,6 +23,7 @@ FPS = 60
 # 색깔들
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+SEMI_SKY = (214, 255, 255)
 
 # 플레이어 이동 속도, 애니메이션 프레임
 pXVel = 1
@@ -28,12 +31,17 @@ animRate = 15
 frameForAnim = 0
 
 # y방향 가속도(중력 가속도로 작용)
-pYAcc = 0.5
+pYAcc = 0.5  # (0.5px * 60^2 / s^2)
+
+# 점프 세기
+jumpF = -8
 
 # 스프라이트 그룹
 tile_group = pygame.sprite.Group()
 enemy_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
+# 실제로 y방향 충돌을 감지하는 스프라이트: 자원을 낭비하므로 디버깅할 때만 사용할 것!
+dummy_group = pygame.sprite.Group()
 
 
 def image_load(path, size):
@@ -49,7 +57,8 @@ class Tile(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = image_load(image_path, tileSize)
         self.x, self.y = x, y
-        self.rect = pygame.Rect(self.x, self.y, tileSize[0], 1)
+        self.rect = pygame.Rect(self.x, self.y, 0, 0)
+        self.mask = pygame.mask.from_surface(self.image)
 
     def set_pos(self, x, y):
         self.x, self.y = x, y
@@ -60,6 +69,30 @@ class Tile(pygame.sprite.Sprite):
 
     def update(self):
         self.rect.x, self.rect.y = self.x, self.y
+
+
+t_tile = Tile("./tiles/stage1_main7.png", 0, 0)
+tile_group.add(t_tile)
+
+
+class dummyPlayer(pygame.sprite.Sprite):
+    def __init__(self, x, y, w, h):
+        pygame.sprite.Sprite.__init__(self)
+        self.x, self.y, self.w, self.h = x, y, w, h
+        self.dy = 0
+        self.dx = 0
+        self.image = image_load("./Player1/walking_dummy2.png", (w, h))
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = self.x, self.y
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+        self.rect.x, self.rect.y = self.x, self.y
+
+    def get_info(self):
+        return self.x, self.y
 
 
 # 플레이어 클래스
@@ -76,6 +109,8 @@ class Player(pygame.sprite.Sprite):
         self.imageNum = 0
         self.image = self.walk_image[self.imageNum]
         self.direction = 0
+        self.mask = pygame.mask.from_surface(self.image)
+        self.jumpState = 0
 
     def move(self, dx, dy, image=False):
         self.x += dx
@@ -92,55 +127,106 @@ class Player(pygame.sprite.Sprite):
         else:
             self.image = self.walk_image[self.imageNum]
 
+        self.mask = pygame.mask.from_surface(self.image)
+
     def pos(self, x, y):
         self.x = x
         self.y = y
+        self.rect.x, self.rect.y = self.x, self.y
+
+    # x 방향으로 움직이는 것이 가능한 지 확인할 것!
+    def x_check_move(self, dx):
+        dummy = dummyPlayer(self.x + dummyMargin, self.y, dummyPlayerSize[0], dummyPlayerSize[1])
+        dummy.move(dx, 0)
+        # dummy_group.add(dummy)
+
+        i = 0
+        while True:
+            if (not (pygame.sprite.collide_mask(dummy, t_tile))) or i > 7:
+                break
+            dummy.move(0, -1)
+            i += 1
+
+        if i > 7:
+            is_ok = 0
+        else:
+            is_ok = 1
+
+        if is_ok == 1:
+            if i != 0:
+                self.y = dummy.y
+                self.rect.y = self.y
+            self.dx = dx
+            self.move(self.dx, 0, frameForAnim == animRate)
+            if pygame.sprite.collide_mask(dummy, t_tile):
+                while pygame.sprite.collide_mask(dummy,t_tile):
+                    if self.dx > 0:
+                        self.x -= 0.1
+                        self.rect.x = self.x
+                    else:
+                        self.x -= 0.1
+                        self.rect.x = self.x
+
+        # dummy_group.draw(window)
+
+    # 중력 구현
+    def check_ground(self):
+        dummy = dummyPlayer(self.x+dummyMargin, self.y, dummyPlayerSize[0], dummyPlayerSize[1])
+
+        if pygame.sprite.collide_mask(dummy, t_tile):
+            if self.dy < 0:
+                while pygame.sprite.collide_mask(dummy, t_tile):
+                    dummy.move(0, 1)
+
+                print(self.y)
+                self.y = dummy.y
+                print(self.y)
+                self.dy = 0
+
+            else:
+                while pygame.sprite.collide_mask(dummy, t_tile):
+                    dummy.move(0, -1)
+
+                self.dy = 0
+                self.y = dummy.y
+                self.jumpState = 1
+
+            self.rect.y = self.y
+
+        # 디버깅 테스트용
+        # dummy_group.add(dummy)
+        # dummy_group.draw(window)
 
     def update(self):
         global frameForAnim
 
-        tileCollide = 0
-        tileY = 0
-
         key = pygame.key.get_pressed()
-        tile_list = pygame.sprite.spritecollide(self, tile_group, False)
 
-        if len(tile_list) > 0:
-            for targetTile in tile_list:
-                if targetTile.y + 1 == self.y + playerSize[1]:
-                    tileCollide = 1
-                    tileY = targetTile.y
-                    break
-                elif targetTile.y + 1 < self.y + playerSize[1]:
-                    tileCollide = 2
-                    tileY = targetTile.y
-                    break
+        self.dy += pYAcc
 
-        # 중력 작용
-        if tileCollide == 0:
-            self.y += self.dy
-            self.dy += pYAcc
-        elif tileCollide == 1 or tileCollide == 2:
-            if self.dy < 0:
-                self.y += 5
-                self.dy = 0
-                tileCollide = 0
-            else:
-                self.y = tileY + 1 - playerSize[1]
-                self.dy = 0
+        # 점프
+        if key[pygame.K_UP] or key[pygame.K_SPACE]:
+            if self.jumpState:
+                self.y -= 1
+                self.rect.y = self.y
 
-        # 이동
+                self.dy = jumpF
+                self.jumpState = 0
+
+        self.y += self.dy
+
+        # 좌우 이동
         if key[pygame.K_RIGHT]:
             if self.direction == 1:
                 self.direction = 0
             self.dx = pXVel
-            self.move(self.dx, 0, frameForAnim == animRate)
+            self.x_check_move(self.dx)
 
         elif key[pygame.K_LEFT]:
             if self.direction == 0:
                 self.direction = 1
             self.dx = -pXVel
-            self.move(self.dx, 0, frameForAnim == animRate)
+            self.x_check_move(self.dx)
 
         else:
             self.imageNum = 0
@@ -150,20 +236,10 @@ class Player(pygame.sprite.Sprite):
             else:
                 self.image = self.walk_image[self.imageNum]
 
-        # x 방향 충돌 감지(현재 작동 안함->고칠것!)
-        if (tileCollide == 1 or tileCollide == 2) and self.dx != 0 and self.dy != 0:
-            self.x += self.dx / pXVel * (-3)
-
-        # 점프
-        if key[pygame.K_UP] or key[pygame.K_SPACE]:
-            if tileCollide == 1 or tileCollide == 2:
-                self.dy = -6
-                self.y -= 1
-
         # 화면 밖으로 떨어지면 위치 초기화
         if self.y > screen[1]:
             self.x = 10
-            self.y = 280
+            self.y = 10
             self.dy = 0
 
         # 프레임 수 초기화
@@ -171,32 +247,24 @@ class Player(pygame.sprite.Sprite):
             frameForAnim = 0
 
         frameForAnim += 1
-        self.rect.x = 3
+
+        # 위치 지정 및 땅 감지
         self.rect.x, self.rect.y = self.x, self.y
+        self.check_ground()
 
 
-player1 = Player(10, 300)
+player1 = Player(10, 200)
 player_group.add(player1)
 
-for i in range(20):
-    a = Tile("./tiles/grassHalfMid.png", 10+i*tileSize[0], 348)
-    tile_group.add(a)
-
-for i in range(10):
-    a = Tile("./tiles/grassHalfMid.png", 100+i*tileSize[0], 280)
-    tile_group.add(a)
-
-for i in range(10):
-    a = Tile("./tiles/grassHalfMid.png", 320+i*tileSize[0], 320)
-    tile_group.add(a)
 
 def tile(posList):
     a = 0
 
+
 while True:
     clock.tick_busy_loop(FPS)
 
-    window.fill((255, 255, 255))
+    window.fill(SEMI_SKY)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit()
@@ -205,10 +273,10 @@ while True:
         '''if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:'''
 
-    player_group.update()
-    player_group.draw(window)
-
     tile_group.update()
     tile_group.draw(window)
+
+    player_group.update()
+    player_group.draw(window)
 
     pygame.display.flip()
